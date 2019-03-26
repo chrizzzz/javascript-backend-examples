7a2f5070c931e5e8a3678c759b6f8d0238c3ad8d
Christina Zenzes <christina.zenzes@codecentric.de>
2019-03-26 19:48:18 +0100

    merge inde.js

--- javascript/mqttExpress/index.js
+++ javascript/mqttExpress/index.js
@@ -1,5 +1,8 @@
-const express = require("express");
-const app = express();
+const mqtt = require("mqtt");
+var app = require("express")();
+var server = require("http").Server(app);
+var socketServer = require("socket.io")(server);
+
 const port = 3000;
 
 app.get("/tea", (req, res) => res.send("Here is you fresh hot cup of tea 🍵"));
@@ -10,6 +13,31 @@ app.get("/water", (req, res) =>
 app.get("/coffee", (req, res) =>
   res.send("Here is you new fresh hot cup of coffee ☕️")
 );
+
+server.listen(3001, () => {
+  console.log("blubber");
+});
+
+socketServer.on("connect", socket => {
+  console.log("connected to socket");
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected");
+  });
+
+  const mqttClient = mqtt.connect("mqtt://localhost:5672");
+
+  mqttClient.on("connect", () => {
+    console.log("connceted with mqtt");
+    mqttClient.subscribe("water");
+    mqttClient.subscribe("tea");
+    mqttClient.subscribe("coffee");
+  });
+
+  mqttClient.on("message", function(topic, message) {
+    console.log(topic, message.toString());
+  });
+});
 
 app.listen(port, () =>
   console.log(`Beverage server is listening on port ${port}!`)
