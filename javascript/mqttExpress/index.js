916bd7422cdb7dc383709191c00f3167c3de9dc6
Anna <anna@MacBook.local>
2019-03-26 19:25:15 +0100

    Fixed Typo

--- javascript/mqttExpress/index.js
+++ javascript/mqttExpress/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/tea", (req, res) => res.send("Here is you fresh hot cup of tea 🍵"));
+app.get("/tea", (req, res) => res.send("Here is your fresh hot cup of tea 🍵"));
 
 app.get("/water", (req, res) =>
-  res.send("Here is you fresh cold glass of water 💧")
+  res.send("Here is your fresh cold glass of water 💧")
 );
 app.get("/coffee", (req, res) =>
-  res.send("Here is you new fresh hot cup of coffee ☕️")
+  res.send("Here is your new fresh hot cup of coffee ☕️")
 );
 
 app.listen(port, () =>
