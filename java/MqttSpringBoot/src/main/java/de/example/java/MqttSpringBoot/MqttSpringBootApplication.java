ee189f02ff122efdf14d4682090585ea23ad2ac5
Christina Zenzes <christina.zenzes@codecentric.de>
2019-03-26 19:48:32 +0100

    add spring boot mqtt example

---
+++ java/MqttSpringBoot/src/main/java/de/example/java/MqttSpringBoot/MqttSpringBootApplication.java
@@ -0,0 +1,13 @@
+package de.example.java.MqttSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqttSpringBootApplication.class, args);
+	}
+
+}
---
+++ java/MqttSpringBoot/src/test/java/de/example/java/MqttSpringBoot/MqttSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package de.example.java.MqttSpringBoot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MqttSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
