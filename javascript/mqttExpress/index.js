5886b316a8e8d1191091b0e196d0b8305cfa3dad
Christina Zenzes <christina.zenzes@codecentric.de>
2019-03-26 19:15:13 +0100

    add water and coffee rest interface

--- javascript/mqttExpress/index.js
+++ javascript/mqttExpress/index.js
@@ -4,6 +4,13 @@ const port = 3000;
 
 app.get("/tea", (req, res) => res.send("Here is you new hot cup of tea "));
 
+app.get("/watter", (req, res) =>
+  res.send("Here is you new hot cup of coffee ")
+);
+app.get("/coffee", (req, res) =>
+  res.send("Here is you new cold glass of watter ")
+);
+
 app.listen(port, () =>
   console.log(`Beverage server is listening on port ${port}!`)
 );
